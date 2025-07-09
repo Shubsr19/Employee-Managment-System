fe5edf8ab3726645392daa4984e1af99cfedc439
Shubh <shubh.srivastav_cs22@gla.ac.in>
2025-07-09 23:26:28 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -12,23 +12,19 @@ dotenv.config();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_CONN)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB:', err));
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/leaves', leaveRoutes);
 app.use('/api/otp', otpRoutes);
 app.use('/api/attendance', attendanceRoutes);
 
-// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ 
@@ -37,7 +33,6 @@ app.use((err, req, res, next) => {
     });
 });
 
-// Test route
 app.get('/api/test', (req, res) => {
     res.json({ message: 'Backend is running!' });
 });
